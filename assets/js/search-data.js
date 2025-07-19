535b4256bc1d1d629c5599b5c2cc881d8b4d6862
timofey256 <54218713+timofey256@users.noreply.github.com>
2025-07-19 23:18:05 +0000

    Deploying to gh-pages from @ timofey256/timofey256.github.io@c28c6f23b94589d03fc191b454d378bc9eab4ee3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,16 +16,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "You can download the PDF to see my CV. Last updated April 2025.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
