070e3698cd9b11419b69a58d7546a3ff02ee34f9
timofey256 <54218713+timofey256@users.noreply.github.com>
2025-08-03 00:33:49 +0000

    Deploying to gh-pages from @ timofey256/timofey256.github.io@87290fe81ebb56ca6ab05c0d84bb2556f20fbf63 🚀

--- shell.nix
+++ shell.nix
@@ -4,5 +4,11 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.python311
     pkgs.python311Packages.jupyter
+    pkgs.nodejs
+    pkgs.prettierd
   ];
+
+  shellHook = ''
+    export PATH=$PWD/node_modules/.bin:$PATH
+  '';
 }
